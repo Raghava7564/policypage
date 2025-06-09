08ae3cbdd4c6b21b87643033b5382efdc46b88ac
Raghava7564 <raghavalakkamraju@gmail.com>
2025-06-09 06:47:20 +0000

    Add all generated UI files for policypage

---
+++ InsuredDetails.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Box } from '@mui/material';
+
+const InsuredDetails = () => (
+  <Box sx={ padding: '20px' }>
+    
+  </Box>
+);
+
+export default InsuredDetails;
