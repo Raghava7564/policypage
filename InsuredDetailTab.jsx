b2faef3d24562bd5cb4c58a4d5a0a404e233915b
Raghava7564 <raghavalakkamraju@gmail.com>
2025-06-09 06:47:22 +0000

    Add all generated UI files for policypage

---
+++ InsuredDetailTab.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Box } from '@mui/material';
+
+const InsuredDetailTab = () => (
+  <Box sx={ padding: '20px' }>
+    
+  </Box>
+);
+
+export default InsuredDetailTab;
