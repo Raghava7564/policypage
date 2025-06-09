84dc301d44c722aa55528fe372ca16a2e1fa94b3
Raghava7564 <raghavalakkamraju@gmail.com>
2025-06-09 06:47:16 +0000

    Add all generated UI files for policypage

---
+++ AgentDetailTab.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Box } from '@mui/material';
+
+const AgentDetailTab = () => (
+  <Box sx={ padding: '20px' }>
+    
+  </Box>
+);
+
+export default AgentDetailTab;
